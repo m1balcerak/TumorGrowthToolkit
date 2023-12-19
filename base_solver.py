b879f2a9333bb1e82cd3161a15b2fd2ba1d76a5d
Michal Balcerak <m1balcerak@gmail.com>
2023-12-19 15:57:38 +0100

    base_solver

--- base_solver.py
+++
@@ -1,8 +0,0 @@
-class BaseSolver:
-    def __init__(self, params):
-        self.params = params
-        # Common initialization for all solvers
-
-    def solve(self):
-        raise NotImplementedError("Solve method must be implemented by the subclass.")
-
